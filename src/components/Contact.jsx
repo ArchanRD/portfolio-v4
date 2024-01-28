d62dfe9f9fe773a2d0043cff34bba506f155a550
ArchanRD <archanrd29@gmail.com>
2024-01-28 16:06:57 +0530

    Contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="mt-20 pb-20">
+      <div>
+        <h2 className="mt-20 font-handlee font-bold text-2xl bg-customPink inline-block">
+          Contact here
+        </h2>
+        <img
+          src="/arrow.png"
+          alt="arrow"
+          width={50}
+          height={50}
+          className="-rotate-90 my-8"
+        />
+        <p className="font-inter text-lg text-gray-800">
+          Let's start a conversation! Feel free to contact me for projects,
+          inquiries or anything else.
+        </p>
+      </div>
+      <form action="" className="mt-8 w-auto">
+        <div className="flex mb-8">
+          <label
+            htmlFor="name"
+            className="bg-customPink font-handlee font-semibold border-l border-gray-800 px-1 text-xl"
+          >
+            Name
+          </label>
+          <input type="text" id="name" name="name" className="border-b w-full border-black bg-transparent focus:outline-none pl-2" required />
+        </div>
+        <div className="flex mb-8">
+          <label
+            htmlFor="email"
+            className="bg-matteYellow font-handlee font-semibold border-l border-gray-800 px-1 text-xl"
+          >
+            Email
+          </label>
+          <input type="email" id="email" name="email" className="border-b w-full border-black bg-transparent focus:outline-none pl-2" required />
+        </div>
+        <div className="flex mb-8">
+          <label
+            htmlFor="message"
+            className="bg-matteBlue font-handlee font-semibold border-l border-gray-800 px-1 text-xl"
+          >
+            Message
+          </label>
+          <input type="text" id="message" name="message" className="border-b w-full border-black bg-transparent focus:outline-none pl-2" required />
+        </div>
+        <button className="rotate-2 bg-black text-white font-inter text-xl px-3 py-2">Send here</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
