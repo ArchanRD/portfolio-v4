84c7575af0b27a247d3f739cafe4a46aa7f7edd7
ArchanRD <archanrd29@gmail.com>
2024-02-02 15:36:43 +0530

    check

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -7,7 +7,7 @@ import WorkExperience from "./components/WorkExperience";
 
 function App() {
   return (
-    <div className="ml-7 pl-5 mr-5 md:px-0 mx-auto bg-dotted bg-no-repeat xl:max-w-7xl xl:mx-auto bg-cover mobile-480:w-[480px] mobile-480:mx-auto sm:bg-dotted4x sm:w-full lg:mx-20 lg:w-auto lg:bg-dottedRegular lg:bg-contain xl:bg-dotted4x xl:bg-cover xl:px-4">
+    <div className="ml-7 pl-5 mr-5 md:px-0 mx-auto bg-no-repeat xl:max-w-7xl xl:mx-auto bg-cover mobile-480:w-[480px] mobile-480:mx-auto sm:w-full lg:mx-20 lg:w-auto  lg:bg-contain xl:bg-cover xl:px-4">
       <Navbar />
       <Hero />
       <WhatIDo />
