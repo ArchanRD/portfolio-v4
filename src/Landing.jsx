f2b33fb366bf1f20897740551d5beecc798af724
ArchanRD <archanrd29@gmail.com>
2024-02-07 12:57:30 +0530

    Removed loading component

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -7,34 +7,21 @@ import Skills from "./components/Skills";
 import Socials from "./components/Socials";
 import WhatIDo from "./components/WhatIDo";
 import WorkExperience from "./components/WorkExperience";
-import Loading from "./components/Loading";
-
-import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setloading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setloading(false);
-    }, 2000);
-  }, []);
-
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
-    <Layout>
-      <Navbar />
-      <Hero />
-      <WhatIDo />
-      <Projects />
-      <WorkExperience />
-      <Skills />
-      <Socials />
-      <Contact />
-    </Layout>
+    <div>
+      <Layout>
+        <Navbar />
+        <Hero />
+        <WhatIDo />
+        <Projects />
+        <WorkExperience />
+        <Skills />
+        <Socials />
+        <Contact />
+      </Layout>
+    </div>
   );
 }
 
