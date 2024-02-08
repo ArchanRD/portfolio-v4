5551ddc5ba7d701ca9bd20aa53089587b352cbfd
ArchanRD <archanrd29@gmail.com>
2024-02-08 21:07:59 +0530

    [bug]: fix repeated anchor tag error <Link />

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -14,18 +14,16 @@ const PageNotFound = () => {
         <h2 className="font-inter text-lg sm:text-xl font-medium text-center text-gray-800">
           404: Page Lost in the Digital Bermuda Triangle
         </h2>
-        <Link>
-          <div className="flex items-center gap-2 mt-4">
-            <span className="material-symbols-outlined text-yellow-400">
-              radio_button_checked
-            </span>
-            <Link to={"/"}>
-              <h1 className="font-handlee text-gray-800 text-2xl font-bold">
-                archan
-              </h1>
-            </Link>
-          </div>
-        </Link>
+        <div className="flex items-center gap-2 mt-4">
+          <span className="material-symbols-outlined text-yellow-400">
+            radio_button_checked
+          </span>
+          <Link to={"/"}>
+            <h1 className="font-handlee text-gray-800 text-2xl font-bold">
+              archan
+            </h1>
+          </Link>
+        </div>
       </div>
     </Layout>
   );
