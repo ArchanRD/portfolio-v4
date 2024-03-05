4fa757352203eb1f27d97f5a42555e3317832a99
ArchanRD <archanrd29@gmail.com>
2024-03-06 01:25:53 +0530

    [update]: changed resume file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Resume from "../PDF's/Archan Resume 2023.pdf"
+import Resume from "../PDF's/Archan Resume 2024.pdf"
 
 const Hero = () => {
   return (
