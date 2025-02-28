77ce2eacd35432ca952dd90a7200e0159c7c848b
ArchanRD <archanrd29@gmail.com>
2025-02-28 10:54:27 +0530

    update resume

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -21,6 +21,7 @@ function App() {
         <Socials />
         <Contact />
       </Layout>
+     
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import Resume from "../PDF's/Archan Resume 2024 updated.pdf";
+import Resume from "../PDF's/Archan Dhrangadharia Resume 2025.pdf";
 
 const Hero = () => {
   const [hasAnimated, setHasAnimated] = useState(false);
